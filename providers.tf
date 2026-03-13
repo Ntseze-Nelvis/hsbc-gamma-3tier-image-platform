3bd3d648243c270125ea44d550ab86bddff7a7cd
Ntseze-Nelvis <ntsezevouffonelvis@gmail.com>
2026-03-13 13:21:07 +0100

    corrected gitlab

--- providers.tf
+++ providers.tf
@@ -12,3 +12,6 @@ provider "aws" {
   region  = "eu-north-1"
   profile = "cloudreality"
 }
+
+# Configure the AWS Provider for the default profile
+# This is used for the S3 backend to store the Terraform state file.
