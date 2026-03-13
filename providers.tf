dd7362b40ef8ca759c314932aeda1135168a7782
Ntseze-Nelvis <ntsezevouffonelvis@gmail.com>
2026-03-13 15:50:33 +0100

    Add final CI/CD pipeline78

--- providers.tf
+++ providers.tf
@@ -12,7 +12,3 @@ provider "aws" {
   region  = "eu-north-1"
   profile = "cloudreality"
 }
-
-# Configure the AWS Provider for the default profile
-# This is used for the S3 backend to store the Terraform state file.
-# THIS IS EDUCATIONAL, C'EST UN MOIYEN DE VISUALISE
