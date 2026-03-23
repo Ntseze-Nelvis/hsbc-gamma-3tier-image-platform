589387ac777d3d8cec65e49e72b9ff2cd9cc8be7
Ntseze-Nelvis <ntsezevouffonelvis@gmail.com>
2026-03-23 19:38:55 +0100

    Add backend configuration block to clear warning

--- providers.tf
+++ providers.tf
@@ -5,8 +5,17 @@ terraform {
       version = "~> 6.0"
     }
   }
+  
+  # Backend configuration - will be configured via CI/CD
+  backend "s3" {
+    # Values will be provided by GitLab CI/CD variables
+    bucket = ""
+    key    = ""
+    region = ""
+  }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
 }
